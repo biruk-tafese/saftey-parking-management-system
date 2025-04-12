ea976d7cb743e7c1c3e279d9cf1c283236b2797a
biruk-tafese <cstafesebiruk23@gmail.com>
2025-04-12 21:48:21 +0300

    first commit ever

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
+        <div className="flex items-center gap-2">
+          <p className="text-2xl font-bold text-gray-800">
+           Parking Management System - Saftey First
+          </p>
+          </div>
+          </div>
+        
+    </>
+  )
+}
+
+export default App
